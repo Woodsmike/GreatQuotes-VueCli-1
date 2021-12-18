e3529b41ed950e0e5a0be2fef559e4ac5fb7954a
Woodsmike <Woodsmike@users.noreply.github.com>
2021-12-18 10:51:46 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>  
+    <div class="container">
+      <new-quote @quoteAdded="newQuote"></new-quote>
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
+      </div> 
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+
+  export default {
+    data: function () {
+      return {
+        quotes: [
+          'Just something to hold text',
+          'Another quote of nothing'
+        ],
+        maxQuotes: 10
+      }
+    },
+    components : {
+      appQuoteGrid : QuoteGrid,
+      newQuote: NewQuote
+    },
+    methods : {
+      newQuote (quote){
+        this.quotes.push(quote)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
